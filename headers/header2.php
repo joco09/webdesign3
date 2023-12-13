55ad5a5dc2f3c5466034f4e40579e9ca962d974f
ArashMehrrostami <137291399+ArashMehrrostami@users.noreply.github.com>
2023-12-13 20:40:17 +0000

    Update header2.php
    compete responsiveness

--- headers/header2.php
+++ headers/header2.php
@@ -26,16 +26,12 @@ require_once '../database_conncetion/config_session.php';
                 Menu
             </button>
         </div>
+    <div class="navbar">
         <ul class="headlinesSection" id="headlinesSection">
             <li><a href="../home_page/index.php">HOME</a></li>
             <li><a href="../classes/classes.php">COURSES AND LESSONS</a></li>
 
             <?php
-
-//            function foo()
-//            {
-//                echo 'ouhgrouewfu';
-//            }
 
             if (!isset($_SESSION['f_n']))
             {
@@ -47,10 +43,39 @@ require_once '../database_conncetion/config_session.php';
                 echo '<li><a href="../profile_page/profile.php"> '. $_SESSION['f_n'] .' </a></li>';
                 echo ' <li><a href="../headers/log_out.php">LOG OUT</a></li>';
             }
-
-
             ?>
         </ul>
-    </header>
+        <ul class="headlinesSectionFullScreen" id="headlinesSectionFullScreen ">
+            <li><a href="../home_page/index.php">HOME</a></li>
+            <li><a href="../classes/classes.php">COURSES AND LESSONS</a></li>
+
+            <?php
+
+            if (!isset($_SESSION['f_n']))
+            {
+                echo '<li><a href="../register_form_page/Register_Form.php">Join</a></li>
+                  <li><a href="../login_form_page/LoginForm.php">Log in</a></li>' ;
+            }
+            else
+            {
+                echo '<li><a href="../profile_page/profile.php"> '. $_SESSION['f_n'] .' </a></li>';
+                echo ' <li><a href="../headers/log_out.php">LOG OUT</a></li>';
+            }
+            ?>
+        </ul>
+    </div>
+</header>
 </body>
+<script>
+    window.document.getElementById("dropdownNavBar").addEventListener("click", function () {
+        document.getElementById("headlinesSection").style.display = "flex";
+        const modal = document.getElementById('headlinesSection');
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+
+            }
+        }
+    })
+</script>
 </html>
