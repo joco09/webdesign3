920c726f62bb9013646fdeb0602a6bcd09f7be8f
ArashMehrrostami <137291399+ArashMehrrostami@users.noreply.github.com>
2023-12-14 23:06:28 +0000

    Update index.php

--- home_page/index.php
+++ home_page/index.php
@@ -19,8 +19,13 @@ session_start();
         <div class = "top-nav">
 
             <img class="logo" alt="logo" src="../images/logo.png">
+            <div class="dropdownNavBar" id="dropdownNavBar">
+                <button>
+                    Menu
+                </button>
+            </div>
 
-            <ul>
+            <ul id="top-nav">
                 <li><a href="../home_page/index.php">Home</a></li>
                 <li><a href="../classes/classes.php">Courses and lessons</a></li>
                 <?php
@@ -32,7 +37,30 @@ session_start();
                 }
                 else
                 {
+                    echo '<li><a href="../profile_page/profile.php"><img src="../images/person-icon.jpg" width="50"></a></li>';
+                }
+
+                ?>
+            </ul>
+
+            <ul id="top-nav-full-screen">
+                <li><a href="../home_page/index.php">Home</a></li>
+                <li><a href="../classes/classes.php">Courses and lessons</a></li>
+                <?php
+
+
+                if (!isset($_SESSION['f_n']))
+                {
+                    // this checks if the Session has been set.
+
+                    echo '<li><a href="../register_form_page/Register_Form.php">Join</a></li>
+                    <li><a href="../login_form_page/LoginForm.php">Log in</a></li>' ;
+                    // this will only show when they are not logged in
+                }
+                else
+                {
                     echo '<li><a href="../profile_page/profile.php"><img src="../images/person-icon.png" width="50"></a></li>';
+                    // this will show a user icon when a user has logged in
                 }
 
                 ?>
@@ -52,44 +80,55 @@ session_start();
             ?>
             </h1>
             <h1>Welcome to game play where we play hard and work hard.</h1>
-            <p>Are you ready to join?</p>
+            <?php
+
+            if(!isset($_SESSION['f_n'])) {
+                echo "<p>Are you ready to join?</p>";
+                //this code will only show when they are not logged in.
+            } ?>
         </div>
         <br>
     </div>
 
     <div class="work-out-classes">
 
-      <div class="class">
-        <label>CHRISTINE</label>
-        <img src="../images/daniel%20trainer.jpg" alt="daniel-trainer">
-        <p>My name is Daniel and I've been a trainer for 7 years. I specialize in endurance training and injury rehabilitation. Join and Book an appointment with me if you fancy.</p>
-        <?php
-        if(!isset($_SESSION['f_n'])){
-            echo "<a href='../register_form_page/Register_Form.php'><button type='button'>Sign Up</button></a>";
-        } ?>
-        
-      </div>
+        <div class="class">
+            <label>DANIEL</label>
+            <img src="../images/daniel%20trainer.jpg" alt="daniel-trainer">
+            <p>My name is Daniel, and I've been a trainer for 7 years. I specialize in endurance training and injury rehabilitation. Join and Book an appointment with me if you fancy.</p>
+            <?php
 
-      <div class="class">
-        <label>ALDOVO</label>
-        <img src="../images/anna%20trainer.jpg" alt="anna-trainer">
-        <p>I am Arasha and have been a trainer ofr 2 years. i specialize in power lifting and crossfit training. If that sounds amazing </p>
-          <?php
-          if(!isset($_SESSION['f_n'])) {
-              echo "<a href='../register_form_page/Register_Form.php'><button type='button'>Sign Up</button></a>";
-          } ?>
-      </div>
+            if(!isset($_SESSION['f_n'])){
+                echo "<a href='../register_form_page/Register_Form.php'><button type='button'>Sign Up</button></a>";
+                //this code will only show when they are not logged in.
+            } ?>
 
-      <div class="class">
-        <label>TREVOR</label>
-        <img src="../images/john%20trainer.jpg" alt="john-trainer">
-        <p>My name is john and i have been a trainer for 8 years. I specialize is combat sport training. I was a professional fighter for 4 years before becoming a trainer. If you want to get first class training then come join and book a class with me.</p>
-        <?php
-        if(!isset($_SESSION['f_n'])){
-            echo "<a href='../register_form_page/Register_Form.php'><button type='button'>Sign Up</button></a>";
-        }
-        ?>
-      </div>
+        </div>
+
+        <div class="class">
+            <label>ARASHA</label>
+            <img src="../images/anna%20trainer.jpg" alt="anna-trainer">
+            <p>I am Arasha and have been a trainer ofr 2 years. i specialize in power lifting and crossfit training. If that sounds amazing </p>
+            <?php
+
+            if(!isset($_SESSION['f_n'])) {
+                echo "<a href='../register_form_page/Register_Form.php'><button type='button'>Sign Up</button></a>";
+                //this code will only show when they are not logged in.
+            } ?>
+        </div>
+
+        <div class="class">
+            <label>JOHN</label>
+            <img src="../images/john%20trainer.jpg" alt="john-trainer">
+            <p>My name is john and i have been a trainer for 8 years. I specialize is combat sport training. I was a professional fighter for 4 years before becoming a trainer. If you want to get first class training then come join and book a class with me.</p>
+            <?php
+
+            if(!isset($_SESSION['f_n'])){
+                echo "<a href='../register_form_page/Register_Form.php'><button type='button'>Sign Up</button></a>";
+                //this code will only show when they are not logged in.
+            }
+            ?>
+        </div>
 
     </div>
     <footer>
@@ -139,10 +178,10 @@ session_start();
         <li><a href="https://www.flaticon.com/free-icons/show-password" title="show password icons">Show password icons created by Stasy - Flaticon</a></li>
         <li><a href="https://www.flaticon.com/free-icons/show-password" title="show password icons">Show password icons created by ZAK - Flaticon</a></li>
         <li><a href="https://www.flaticon.com/free-icons/search" title="search icons">Search icons created by Freepik - Flaticon</a></li>
-          <li><a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by Freepik - Flaticon</a></li>
       </ul>
     </footer>
-    <script src="script.js"></script>
+    </footer>
+    <script src="scripts.js"></script>
 
 </body>
 </html>
